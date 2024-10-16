f0486cc2a36b9798dc7cb5d4006d7e991147923b
Manu254rce <straussmoabit@gmail.com>
2024-10-16 15:48:36 +0300

    chore:initialized new repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Calendar from './components/calendar/calendar';
+import ActionPane from './components/action_pane/action_pane';
 
 function App() {
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const handleAddEvent = (newEvent: Event) => {
+    setEvents([...events, newEvent])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='w-screen h-screen p-4 grid grid-cols-4 gap-4 bg-gradient-to-br from-blue-300 to-white'>
+      <ActionPane onAddEvent={handleAddEvent}/>
+      <Calendar/>
+    </main>
   );
 }
 
