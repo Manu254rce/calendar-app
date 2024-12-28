30eaf907a7cb90bd633a4ba58d987d431276c778
Manu254rce <straussmoabit@gmail.com>
2024-12-28 18:10:55 +0300

    todo:gonna address errors to do with saving passwords/ updating prefs

--- frontend/src/api/axios_config.ts
+++ frontend/src/api/axios_config.ts
@@ -6,29 +6,27 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
-    
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    
-    return config;
+      const token = localStorage.getItem('token');
+      if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
   },
   (error) => {
-    return Promise.reject(error);
+      return Promise.reject(error);
   }
 );
 
 api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
+  (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token'); 
-      window.location.href = '/login'; 
-    }
-    return Promise.reject(error);
+      if (error.response && error.response.status === 401) {
+          // Token is invalid or expired
+          localStorage.removeItem('token');
+          localStorage.removeItem('user');
+          window.location.href = '/login';
+      }
+      return Promise.reject(error);
   }
 );
 
