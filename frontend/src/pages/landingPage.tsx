2d7ac1624330ec49d1c02b78561d8f4e66e9649d
Z'PHYR <emmanuellamira20@outlook.com>
2025-09-27 21:10:41 +0300

    chore: unstaged changes

--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -7,7 +7,7 @@ const LandingPage = () => {
     <main className="bg-gradient-to-br from-blue-900 to-fuchsia-800 min-h-screen flex justify-center items-center
                                           px-4 sm:px-6 lg:px-8">
       <div className="text-center flex flex-col w-full max-w-md justify-between space-y-4">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl text-white mb-4">Calendar App</h1>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl text-white mb-4">KBC JournaBuddy</h1>
         <Link to="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button className="flex items-center justify-center w-full">
             <LogIn className="mr-2" size={20} />
